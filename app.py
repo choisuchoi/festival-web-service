f6b86422868622c4eeb14af90cf7cb249e24d01f
choisuchani <suchan7509@gs.cwnu.ac.kr>
2023-12-05 16:34:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['JSON_AS_ASCII'] = False
 
 #load main page data
 #id, x, y 데이터 만을 리턴한다.
-@app.route('/api')
+@app.route('/api/')
 def home():
     festival_info = festival_db.get_all_festival_info()
     total_list = list()
@@ -15,7 +15,7 @@ def home():
         #data_dict = { 'id' :item[0]}
         #place_list = {'x': item[2], 'y' : item[1]}
         #data_dict['location'] = place_list
-        data_dict = {'id' : item[0], 'name' : item[1], 'address' : item[2], 'x' : item[4], 'y' : item[3], 'place_n' : item[5], 'begin_date' : item[6], 'end_date' : item[7], 'detail' : item[8]}
+        data_dict = {'id' : item[0], 'name' : item[1], 'address' : item[2], 'x' : item[4], 'y' : item[3], 'place_n' : item[5], 'begin_date' : item[6], 'end_date' : item[7], 'detail' : item[8], 'img_src' : "image/" + item[0] + ".jpg"}
         total_list.append(data_dict)
     return total_list
 
@@ -32,7 +32,7 @@ def get_festival_data():
         for i, item in enumerate(festival_info) :
             data_dict = {'id' : item[0], 'name' : item[1], 'address' : item[2], 'x' : item[4], 'y' : item[3], 'place_n' : item[5], 'begin_date' : item[6], 'end_date' : item[7], 'detail' : item[8]}
             total_list.append(data_dict)
-        return festival_info
+        return total_list
 
 #search festival info 
 # 특정 검색 인덱스를 통하여 DB에서 검색을 실시해서 해당 결과에 맞는 축제들을 return한다.
